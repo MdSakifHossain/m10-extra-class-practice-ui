b3c4e9c25ce5219e4da87a52e89dab86802fdde1
MdSakifHossain <sakif015sakif@gmail.com>
2026-02-01 00:52:39 +0600

    feat(ui): implement order form logic and submission handler

--- src/pages/DetailsPage.tsx
+++ src/pages/DetailsPage.tsx
@@ -18,18 +18,51 @@ import {
 import { Field, FieldGroup, FieldLabel } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { formatDate } from "@/lib/date";
+import { useAuth } from "@/contexts/authContext/AuthProvider";
+import { Textarea } from "@/components/ui/textarea";
 
 const DetailsPage = () => {
   const { id: productID } = useParams();
+  const { user } = useAuth();
   const [loading, setLoading] = useState(true);
   const [details, setDetails] = useState({});
   const [error, setError] = useState(null);
   const [orderDialogueStatus, setOrderDialogueStatus] = useState(false);
   const [quantity, setQuantity] = useState(1);
 
-  const handleOrder = (e) => {
+  const handleOrder = async (e) => {
     e.preventDefault();
-    console.log("form-submition-initiated");
+
+    // garbage collection
+    const formEl = e.target;
+    const form = new FormData(formEl);
+
+    const formData = {
+      productName: form.get("productName"),
+      buyerName: form.get("buyerName"),
+      price: Number(form.get("price")),
+      quantity: Number(form.get("quantity")),
+      address: form.get("address"),
+      phoneNumber: form.get("phoneNumber"),
+      email: form.get("email"),
+      additionalNote: form.get("additionalNote"),
+      productID,
+      date: Date.now(),
+    };
+
+    // api call
+    try {
+      const { data: apiRes } = await axios.post(
+        "http://localhost:3000/orders",
+        formData,
+      );
+      notify.success({ description: apiRes.message });
+    } catch (err) {
+      setError(err);
+      notify.danger({ title: err.code, description: err.message });
+    } finally {
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
@@ -94,7 +127,7 @@ const DetailsPage = () => {
 
       {/* Invisbel content */}
       <Dialog open={orderDialogueStatus} onOpenChange={setOrderDialogueStatus}>
-        <DialogContent className="sm:max-w-sm">
+        <DialogContent className="sm:max-w-lg border-4">
           <DialogHeader>
             <DialogTitle>Order Form</DialogTitle>
             <DialogDescription>
@@ -102,7 +135,10 @@ const DetailsPage = () => {
             </DialogDescription>
           </DialogHeader>
 
-          <form className="w-full max-w-sm" onSubmit={(e) => handleOrder(e)}>
+          <form
+            className="w-full max-w-lg no-scrollbar max-h-[70svh] overflow-y-auto px-4"
+            onSubmit={(e) => handleOrder(e)}
+          >
             <FieldGroup>
               {/* Product Name */}
               <Field>
@@ -111,19 +147,38 @@ const DetailsPage = () => {
                   id="product-name"
                   type="text"
                   placeholder="Product Name"
+                  defaultValue={details?.name}
+                  readOnly
+                  required
+                  name="productName"
                 />
               </Field>
 
               {/* Buyer Name */}
               <Field>
                 <FieldLabel htmlFor="buyer-name">Buyer Name</FieldLabel>
-                <Input id="buyer-name" type="text" placeholder="Buyer Name" />
+                <Input
+                  id="buyer-name"
+                  type="text"
+                  placeholder="Buyer Name"
+                  defaultValue={user?.displayName}
+                  required
+                  name="buyerName"
+                />
               </Field>
 
               {/* Product Price */}
               <Field>
                 <FieldLabel htmlFor="product-price">Price</FieldLabel>
-                <Input id="product-price" type="number" placeholder="Price" />
+                <Input
+                  id="product-price"
+                  type="number"
+                  placeholder="Price"
+                  readOnly
+                  required
+                  defaultValue={details?.price}
+                  name="price"
+                />
               </Field>
 
               {/* Quantity Big number */}
@@ -165,31 +220,71 @@ const DetailsPage = () => {
                   min={1}
                   value={quantity === 0 ? "" : quantity}
                   onChange={(e) => setQuantity(Number(e.target.value) || 0)}
+                  required
+                  name="quantity"
                 />
               </Field>
 
               {/* Address */}
               <Field>
                 <FieldLabel htmlFor="form-address">Address</FieldLabel>
-                <Input id="form-address" type="text" placeholder="Address" />
+                <Input
+                  id="form-address"
+                  type="text"
+                  placeholder="Address"
+                  required
+                  name="address"
+                />
               </Field>
 
               {/* Phone Number */}
               <Field>
                 <FieldLabel htmlFor="form-phone">Phone</FieldLabel>
-                <Input id="form-phone" type="tel" placeholder="Phone" />
+                <Input
+                  id="form-phone"
+                  type="tel"
+                  placeholder="Phone"
+                  defaultValue={user.phoneNumber ? user.phoneNumber : ""}
+                  required
+                  name="phoneNumber"
+                />
               </Field>
 
               {/* Email */}
               <Field>
                 <FieldLabel htmlFor="form-email">Email</FieldLabel>
-                <Input id="form-email" type="email" placeholder="Email" />
+                <Input
+                  id="form-email"
+                  type="email"
+                  placeholder="Email"
+                  readOnly
+                  required
+                  defaultValue={user?.email}
+                  name="email"
+                />
+              </Field>
+
+              {/* Additional Notes */}
+              <Field>
+                <FieldLabel htmlFor="form-additional-note">
+                  Additional Note
+                </FieldLabel>
+                <Textarea
+                  id="form-additional-note"
+                  placeholder="Your message here"
+                  name="additionalNote"
+                  className="min-h-24"
+                />
               </Field>
 
               {/* Buttons */}
               <Field orientation="horizontal" className="grid grid-cols-2">
-                <Button type="button" variant="outline">
-                  Cancel
+                <Button
+                  type="reset"
+                  variant="outline"
+                  onClick={() => setOrderDialogueStatus(false)}
+                >
+                  Close
                 </Button>
 
                 <Button type="submit" variant="default">
