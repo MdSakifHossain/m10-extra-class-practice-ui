0b6127a2da5c118f43481648ebd22b4a74924f76
MdSakifHossain <sakif015sakif@gmail.com>
2026-02-04 20:07:01 +0600

    refactor(ui): update service card component

--- src/components/app/features/ProductCard.tsx
+++ src/components/app/features/ProductCard.tsx
@@ -13,39 +13,32 @@ import { Link } from "react-router";
 
 const ProductCard = ({ service }) => {
   return (
-    <div className="max-w-md rounded-xl">
-      <div className="flex items-center justify-center">
-        <img
-          src={service.image_url}
-          alt={service.name}
-          className="aspect-video object-cover rounded-t-md"
-        />
-      </div>
-      <Card className="rounded-t-none">
-        <CardHeader>
-          <CardTitle>{service.name}</CardTitle>
-          <CardDescription className="flex items-center gap-2">
-            <Badge variant="outline" className="rounded-sm text-xs">
-              {service.category}
-            </Badge>
-          </CardDescription>
-        </CardHeader>
-        {/* <CardContent>
-          <p>{service.description}</p>
-        </CardContent> */}
-        <CardFooter className="justify-between gap-3 max-sm:flex-col max-sm:items-stretch">
-          <div className="flex flex-col">
-            <span className="text-sm font-medium uppercase">Price</span>
-            <span className="text-xl font-semibold">${service.price}</span>
-          </div>
-          <Link to={`/details/${service._id}`}>
-            <Button size="lg" variant="outline">
-              Details
-            </Button>
-          </Link>
-        </CardFooter>
-      </Card>
-    </div>
+    <Card className="border shadow-none rounded-none">
+      <img
+        src={service.image_url}
+        alt={service.name}
+        className="aspect-video object-cover rounded-none"
+      />
+      <CardHeader>
+        <CardTitle className="flex items-center gap-3">
+          {service.name}
+          <Badge variant="default" className="rounded-full text-xs">
+            {service.category}
+          </Badge>
+        </CardTitle>
+      </CardHeader>
+
+      <CardFooter className="justify-between gap-3 max-sm:flex-col max-sm:items-stretch">
+        <div className="flex flex-col">
+          <span className="text-3xl font-semibold">${service.price}</span>
+        </div>
+        <Link to={`/details/${service._id}`}>
+          <Button size="lg" variant="outline" className="shadow-none">
+            Details
+          </Button>
+        </Link>
+      </CardFooter>
+    </Card>
   );
 };
 
