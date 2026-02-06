e7c39712b04d33df48d610d91fc105db2e2d6eab
MdSakifHossain <sakif015sakif@gmail.com>
2026-02-06 21:32:18 +0600

    feat(Navbar): add conditional authentication-based navigation links with icons

--- src/components/app/layout/Navbar.tsx
+++ src/components/app/layout/Navbar.tsx
@@ -6,16 +6,21 @@ import { ModeToggle } from "../appearance/mode-toggle";
 import { Button } from "@/components/ui/button";
 import { navlinks } from "@/constants/";
 import { ButtonGroup } from "@/components/ui/button-group";
+import { useAuth } from "@/contexts/authContext/AuthProvider";
+import { useEffect, useState } from "react";
+import { TriangleAlert } from "lucide-react";
 
 const Navbar = () => {
   const { site_title, setCommandOpen } = useAppConfig();
+  const { user } = useAuth();
+  const isLoggedIn = !!user;
 
   return (
     <>
       <div className="flex items-center justify-between px-5 py-3 lg:px-24 lg:py-4 border-b">
         <SiteTitleAndLogo title={site_title} />
 
-        <NavLinksGroup />
+        <NavLinksGroup navlinks={navlinks} isLoggedIn={isLoggedIn} />
 
         <NavbarEnding setCommandOpen={setCommandOpen} />
       </div>
@@ -39,12 +44,26 @@ function SiteTitleAndLogo({ title }) {
   );
 }
 
-function NavLinksGroup() {
+function NavLinksGroup({ navlinks, isLoggedIn }) {
+  const [links, setLinks] = useState([]);
+
+  useEffect(() => {
+    const doTheThing = () => {
+      if (!isLoggedIn)
+        return setLinks(navlinks.filter((item) => item.hidden !== true));
+
+      setLinks(navlinks.filter((item) => item.allowedAfterLogin !== false));
+    };
+    doTheThing();
+  }, [navlinks, isLoggedIn]);
+
   return (
     <ButtonGroup>
-      {navlinks.map((item, index) => (
+      {links.map((item, index) => (
         <NavLink key={index} to={item.link}>
-          <Button variant="outline">{item.text}</Button>
+          <Button variant="outline">
+            {item.icon ? <item.icon /> : <TriangleAlert />} {item.text}
+          </Button>
         </NavLink>
       ))}
     </ButtonGroup>
--- src/constants/navlinks.ts
+++ src/constants/navlinks.ts
@@ -1,10 +1,52 @@
+import {
+  Home,
+  LogIn,
+  PlusIcon,
+  ScrollText,
+  SquareRoundCorner,
+  Tags,
+  UserIcon,
+} from "lucide-react";
+
 export const navlinks = [
   {
     text: "Home",
+    icon: Home,
     link: "/",
   },
   {
     text: "Services",
+    icon: Tags,
     link: "/services",
+  },
+  {
+    text: "Create",
+    icon: PlusIcon,
+    link: "/create",
+    hidden: true,
+  },
+  {
+    text: "My Services",
+    icon: ScrollText,
+    link: "/my-services",
+    hidden: true,
+  },
+  {
+    text: "My Orders",
+    icon: SquareRoundCorner,
+    link: "/my-orders",
+    hidden: true,
+  },
+  {
+    text: "Profile",
+    icon: UserIcon,
+    link: "/profile",
+    hidden: true,
+  },
+  {
+    text: "Log In",
+    icon: LogIn,
+    link: "/login",
+    allowedAfterLogin: false,
   },
 ];
